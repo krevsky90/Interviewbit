851c04dfc46c95cc635164f60302c1de6dfc9f4e
krevsky <krevskygr@frtk.ru>
2020-09-28 00:40:47 +0300

    add binary-representation solution

---
+++ default/src/math/factorization/binary_representation/MySolution.java
@@ -0,0 +1,16 @@
+package math.factorization.binary_representation;
+
+public class MySolution {
+    public String findDigitsInBinary(int A) {
+        StringBuilder sb = new StringBuilder();
+        while (A >= 2) {
+            int delta = A % 2;
+            A = A / 2;
+            sb.append(delta);
+        }
+
+        sb.append(A);
+        return sb.reverse().toString();
+    }
+}
+
